77214a9263e2600b0f13e1df9b65e33b17bbd02d
houda <houdadalar123@gmail.com>
2022-10-27 13:20:47 +0100

    update

--- src/main/java/com/esprit/Dependency/tpDependencyInjection/dao/IToDoDao.java
+++ src/main/java/com/esprit/Dependency/tpDependencyInjection/dao/IToDoDao.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface IToDoDao  extends JpaRepository<matiere,Long> {
+public interface IToDoDao //extends JpaRepository<matiere,Long>
+{
 	public List<String> getCoursesList();
 }
